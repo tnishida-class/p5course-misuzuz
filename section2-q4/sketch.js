f64c9170848358c0c318d43228cf80d6b11b0569
misuzuz <92075118+misuzuz@users.noreply.github.com>
2021-10-25 14:45:19 +0900

    Change section2-q4

--- section2-q4/sketch.js
+++ section2-q4/sketch.js
@@ -2,13 +2,19 @@
 function setup() {
   const blue = color(0, 51, 160);
   createCanvas(270, 180);
-  // noStroke();
+  noStroke();
   background(255);
 
   let d = height / 9; // 縞1本の太さ
 
   for(let i = 0; i < 9; i++){
     // BLANK[1] (hint: 縞の色を交互に変えるには2で割った余りを使おう)
+   if(i % 2 == 0){
+     fill(blue);
+   }
+   else{
+     fill(255);
+   }
     rect(0, i * d, width, (i + 1) * d);
   }
 
@@ -18,4 +24,6 @@ function setup() {
 
   fill(255);
   // BLANK[2] (hint: 白い十字を描くには rect を二つ描こう)
+  rect(2/5 * size, 0, d, size);
+  rect(0, 2/5 * size, size, d);
 }
