bf2c2766c29e32d75463ed9878689bc2b6734ca9
misuzuz <92075118+misuzuz@users.noreply.github.com>
2021-11-01 13:46:30 +0900

    Change section 3-q1

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -12,5 +12,13 @@ function draw(){
   background(160, 192, 255);
   count = (count + 1) % cycle;
   // BLANK[1]
-  ellipse(width / 2, height / 2, size);
+  if(count > 40 && count < 60){
+  ellipse(width / 2, height / 2, count);
+}
+   else if(count < 40){
+     ellipse(width / 2, height / 2, count / 2);
+   }
+   else {
+     ellipse(width / 2, height / 2, 100 - count);
+  }
 }
