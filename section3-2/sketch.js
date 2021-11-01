d5228eba91555d6638c8b5c0c4246feee8e00437
misuzuz <92075118+misuzuz@users.noreply.github.com>
2021-11-01 15:09:36 +0900

    Change section 3-2

--- section3-2/sketch.js
+++ section3-2/sketch.js
@@ -20,15 +20,20 @@ function draw(){
   // 重力（コメント機能でオンオフ切り替えて実行してみましょう）
   vy = constrain(vy + g, -vyMax, vyMax);
 
+  //練習問題
+  if (keyIsDown(LEFT_ARROW)){ x -= 5; }
+  if(keyIsDown(RIGHT_ARROW)){ x += 5; }
+  if(keyIsDown(" ".charCodeAt(0))){ x += 10; }
+
   // 端の処理パターン (1) 反対側から出てくる
-  // if(x > width){ x = 0; }
-  // else if(x < 0){ x = width; }
-  // if(y > height){ y = 0; }
-  // if(y < 0){ y = height; }
+   //if(x > width){ x = 0; }
+   //else if(x < 0){ x = width; }
+   //if(y > height){ y = 0; }
+   //if(y < 0){ y = height; }
 
 　// 端の処理パターン (2) 跳ね返る
   if(x < 0 || x > width){ vx = -1 * vx; }
-  if(y > height){ vy = -1 * vy; }
+  if(y < 0 || y > height){ vy = -1 * vy; }
   x = constrain(x, 0, width);
   y = constrain(y, 0, height);
 }
