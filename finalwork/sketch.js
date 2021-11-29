762468335c6c2b6dd53249c8f59e3a7ffb00222e
misuzuz <92075118+misuzuz@users.noreply.github.com>
2021-11-29 21:55:59 +0900

    Change finalwork

--- finalwork/sketch.js
+++ finalwork/sketch.js
@@ -1,17 +1,10 @@
 // 最終課題を制作しよう
 
-// let img;
-// function preload(){
-//   img = loadImage ('italiangirl.jpg');
-// }
 function setup(){
   createCanvas(windowWidth, windowHeight);
-  // image(img, 0, 0);
-   shuffle(questionLv1, true);
-   shuffle(questionLv2, true);
-   shuffle(questionLv3, true);
-
-
+  shuffle(questionLv1, true);
+  shuffle(questionLv2, true);
+  shuffle(questionLv3, true);
 }
 
 function windowResized(){
@@ -68,39 +61,39 @@ function draw(){
   background(0, 128, 0);
   textAlign(CENTER);
 
-   push();
-   strokeWeight(4);
-   stroke(255, 0, 0);
-   fill(255);
-   textSize(25);
-   text("イタリア語 名詞の性・数一致ゲーム", width * 0.5, height * 0.1);
-   pop();
+  push();
+  strokeWeight(4);
+  stroke(255, 0, 0);
+  fill(255);
+  textSize(25);
+  text("イタリア語 名詞の性・数一致ゲーム", width * 0.5, height * 0.1);
+  pop();
 
   push();
   fill(0);
   textSize(15);
   let t1;
   t1 = "あそびかた…下の単語が 男性単数名詞なら'm'　男性複数名詞なら'M' 女性単数名詞なら'f'　女性複数名詞なら'F' をおしてください。Enterキーで次の問題に移ります"
-  text(t1, width * 0.4, 70, 200, 200);
+  text(t1, width * 0.4, height * 0.1 + 20, 200, 200);
   pop();
   push();
   textSize(10);
   let t2;
   t2 = "(ただし、日本語は単数形・複数形ともに単数表記しています。) イタリア語初心者の方は、右矢印ボタンで説明ページに飛べます。"
-  text(t2, width * 0.4, 150, 200, 200);
+  text(t2, width * 0.4, height * 0.1 + 100, 200, 200);
   pop();
 
 
   textSize(32);
   if(s < questionLv1.length){
-   text(questionLv1[s].w, width * 0.5, height * 0.4);
-   push();
-   strokeWeight(2);
-   stroke(255, 0, 0);
-   fill(255);
-   text("Level.1", width * 0.5 - 220, height * 0.4);
-   pop();
-   }
+    text(questionLv1[s].w, width * 0.5, height * 0.4);
+    push();
+    strokeWeight(2);
+    stroke(255, 0, 0);
+    fill(255);
+    text("Level.1", width * 0.5 - 220, height * 0.4);
+    pop();
+  }
   else if(s < questionLv1.length + questionLv2.length){
     text(questionLv2[s - questionLv1.length].w, width *0.5, height * 0.4);
     push();
@@ -123,8 +116,8 @@ function draw(){
     noStroke();
     for(let x = 0; x < width; x = x + width * 1 / 3){
       if(x == 0){fill(46, 139, 87);}
-       else if(x == width * 1 / 3){fill(255);}
-       else {fill(178, 34,34);}
+      else if(x == width * 1 / 3){fill(255);}
+      else {fill(178, 34,34);}
       rect(x, 0, width * 1 / 3, height);
     }
     text("completato!", width * 0.5, height * 0.5);
@@ -156,29 +149,29 @@ function currentQuestion(){
 function keyPressed(){
   const q = currentQuestion();
   if(keyCode == ENTER){
-  s = s + 1;
-  correct = false;
-  incorrect = false;}
-  if(key == "m" && q.g == "m" && q.n == "s")
-  {correct = true;}
-  else if(key == "M" && q.g == "m" && q.n == "p")
-  {correct = true;}
-  else if(key == "f" && q.g == "f" && q.n == "s")
-  {correct = true;}
-  else if(key == "F" && q.g == "f" && q.n == "p")
-  {correct = true;}
-  else if (key == "f" || key == "M" || key == "F" && q.g == "m" && q.n == "s")
-  {incorrect = true;}
-  else if (key == "m" || key == "f" || key == "F" && q.g == "M" && q.n == "p")
-  {incorrect = true;}
-  else if (key == "m" || key == "M" || key == "F" && q.g == "f" && q.n == "s")
-  {incorrect = true;}
-  else if (key == "m" || key == "M" || key == "f" && q.g == "m" && q.n == "p")
-  {incorrect = true;}
-  if (keyCode == RIGHT_ARROW){
-    window.open("https://docs.google.com/document/d/1pvMtecS153qSxtqJntLV7RLIAtJf03s-LfitRcak7Yo/edit");
+    s = s + 1;
+    correct = false;
+    incorrect = false;}
+    if(key == "m" && q.g == "m" && q.n == "s")
+    {correct = true;}
+    else if(key == "M" && q.g == "m" && q.n == "p")
+    {correct = true;}
+    else if(key == "f" && q.g == "f" && q.n == "s")
+    {correct = true;}
+    else if(key == "F" && q.g == "f" && q.n == "p")
+    {correct = true;}
+    else if (key == "f" || key == "M" || key == "F" && q.g == "m" && q.n == "s")
+    {incorrect = true;}
+    else if (key == "m" || key == "f" || key == "F" && q.g == "M" && q.n == "p")
+    {incorrect = true;}
+    else if (key == "m" || key == "M" || key == "F" && q.g == "f" && q.n == "s")
+    {incorrect = true;}
+    else if (key == "m" || key == "M" || key == "f" && q.g == "m" && q.n == "p")
+    {incorrect = true;}
+    if (keyCode == RIGHT_ARROW){
+      window.open("https://docs.google.com/document/d/1pvMtecS153qSxtqJntLV7RLIAtJf03s-LfitRcak7Yo/edit");
+    }
   }
-}
 
 
   function thmbsup(rx, ry){
@@ -208,31 +201,31 @@ function keyPressed(){
     pop();
   }
 
-    function thmbsdown(rx, ry){
-      translate(width * 2 / 3, height * 7 / 5);
-      rotate(PI)
-      push();
-      noStroke();
-      fill(255, 215, 0);
-      rect(rx, ry + 10, 50, 80, 20, 0, 0, 0);//親指までのしかく
-      rect(rx + 50, ry, 150, 80, 20, 30, 20, 0);//人差し指までのしかく
-      rect(rx, ry + 80, 160, 100, 0, 30, 20, 15);//中指・薬指のしかく
-      rect(rx + 20, ry + 160, 130, 40, 0, 10, 10, 10);//小指のしかく
-      triangle(rx + 12, ry + 15, rx + 35, ry - 15, rx + 35, ry + 15 );//親指までのさんかく
-      rect(rx + 35, ry - 100, 50, 120, 20, 20, 0, 0);//親指
-      pop();
-      push();
-      fill (255, 215, 0);
-      stroke(255, 165, 0);
-      strokeWeight(5);
-      rect(rx + 100, ry + 40, 100, 40, 20, 10, 10, 20);//人差し指
-      rect(rx + 100, ry + 80, 95, 40, 20, 10, 10, 20);//中指
-      rect(rx + 100, ry + 120, 90, 40, 20, 10, 10, 20);//薬指
-      rect(rx + 100, ry + 160, 80, 40, 20, 10, 10, 20);//小指
-      pop();
-      push();
-      fill(255, 215, 0);
-      stroke(255, 215, 0);
-      rect(rx + 150, ry, 50, 70, 0, 30, 20, 0);//人差し指の線消し
-      pop();
-    }
+  function thmbsdown(rx, ry){
+    translate(width * 2 / 3, height * 7 / 5);
+    rotate(PI)
+    push();
+    noStroke();
+    fill(255, 215, 0);
+    rect(rx, ry + 10, 50, 80, 20, 0, 0, 0);//親指までのしかく
+    rect(rx + 50, ry, 150, 80, 20, 30, 20, 0);//人差し指までのしかく
+    rect(rx, ry + 80, 160, 100, 0, 30, 20, 15);//中指・薬指のしかく
+    rect(rx + 20, ry + 160, 130, 40, 0, 10, 10, 10);//小指のしかく
+    triangle(rx + 12, ry + 15, rx + 35, ry - 15, rx + 35, ry + 15 );//親指までのさんかく
+    rect(rx + 35, ry - 100, 50, 120, 20, 20, 0, 0);//親指
+    pop();
+    push();
+    fill (255, 215, 0);
+    stroke(255, 165, 0);
+    strokeWeight(5);
+    rect(rx + 100, ry + 40, 100, 40, 20, 10, 10, 20);//人差し指
+    rect(rx + 100, ry + 80, 95, 40, 20, 10, 10, 20);//中指
+    rect(rx + 100, ry + 120, 90, 40, 20, 10, 10, 20);//薬指
+    rect(rx + 100, ry + 160, 80, 40, 20, 10, 10, 20);//小指
+    pop();
+    push();
+    fill(255, 215, 0);
+    stroke(255, 215, 0);
+    rect(rx + 150, ry, 50, 70, 0, 30, 20, 0);//人差し指の線消し
+    pop();
+  }
